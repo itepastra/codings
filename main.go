46afbd0c70b2c25feb84c6f0533004844e88fc54
Noa Aarts <noa@voorwaarts.nl>
2024-09-25 20:55:08 +0200

    feat: tui v1

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"os"
+	"sync"
 
+	"github.com/charmbracelet/huh"
 	"github.com/icza/bitio"
 	"github.com/itepastra/codings/huffman"
 	"github.com/itepastra/codings/lz77"
@@ -24,6 +28,13 @@ var lengthBits = flag.Int("length", 8, "how many bits should be used for the len
 var offsetBits = flag.Int("offset", 16, "how many bits should be used for the offset in lz77 (up to 63)")
 
 var debug = flag.Bool("debug", false, "should enable debug output")
+
+const ENCODE_TYPES = 4
+
+type encoded struct {
+	codec string
+	data  []byte
+}
 
 func main() {
 	flag.Parse()
@@ -73,5 +84,84 @@ func main() {
 			return
 		}
 		writer.Flush()
+	} else {
+		var file string
+		var chosenType string
+		var fileText []byte
+		form := huh.NewForm(
+			huh.NewGroup(
+				huh.NewFilePicker().
+					Height(10).
+					Value(&file).
+					Key("original").
+					Title("Pick what file you want to encode"),
+				huh.NewNote().TitleFunc(func() string {
+					f, err := os.Open(file)
+					if err != nil {
+						return "There was an error opening the file"
+					}
+					defer f.Close()
+
+					text, err := io.ReadAll(f)
+					if err != nil {
+						return "there was an error reading the file"
+					}
+
+					fileText = text
+					return fmt.Sprintf("%s has size %dB", file, len(fileText))
+				}, &file),
+				huh.NewSelect[string]().
+					Value(&chosenType).
+					Height(10).
+					OptionsFunc(func() []huh.Option[string] {
+						options := []huh.Option[string]{}
+						ch := make(chan encoded, ENCODE_TYPES)
+						wg := sync.WaitGroup{}
+
+						for i := range ENCODE_TYPES {
+							wg.Add(1)
+							go func(instance int, channel chan encoded, waitgroup *sync.WaitGroup) {
+								defer waitgroup.Done()
+								var name string
+								buf := bytes.NewBuffer([]byte{})
+								bitio := bitio.NewWriter(buf)
+								switch instance {
+								case 0:
+									name = "huffman"
+									huffman.Encode(fileText, bitio)
+								case 1:
+									name = "lz77 (16, 8)"
+									lz77.Encode(fileText, 16, 8, bitio)
+								case 2:
+									name = "lz77 (8, 4)"
+									lz77.Encode(fileText, 8, 4, bitio)
+								case 3:
+									name = "lz77 (4, 4)"
+									lz77.Encode(fileText, 4, 4, bitio)
+								}
+								_, err := bitio.Align()
+								if err != nil {
+									log.Warningf("encoding error with %s (%e)", name, err)
+								}
+								channel <- encoded{name, buf.Bytes()}
+								return
+							}(i, ch, &wg)
+						}
+
+						wg.Wait()
+						close(ch)
+						for enc := range ch {
+							options = append(options, huh.NewOption(fmt.Sprintf("%s (%dB)", enc.codec, len(enc.data)), string(enc.data)))
+						}
+
+						return options
+					}, &fileText),
+			),
+		)
+
+		err := form.Run()
+		if err != nil {
+			log.Critical(err)
+		}
 	}
 }
